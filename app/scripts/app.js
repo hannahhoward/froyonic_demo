32947b78ff525e385df50ba71bde6900e79eda61
Hannah Howard <hannah@hannahhoward.net>
2014-08-04 22:41:15 -0700

    First view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,38 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('Froyonic', ['ionic'])
+var yogurtApp = angular.module('Froyonic', ['ionic'])
+.config( function($stateProvider, $urlRouterProvider, $httpProvider){
+  var defaults = $httpProvider.defaults.headers;
 
+  defaults.patch = defaults.patch || {};
+  defaults.patch['Content-Type'] = 'application/json';
+  defaults.common['Accept'] = 'application/json';
+
+  $stateProvider
+    .state('index', {
+      url: "/yogurts",
+      templateUrl: "templates/index.html",
+      controller: 'YogurtsCtrl'
+    })
+    .state('new', {
+      url: "/yogurts/new",
+      templateUrl: "templates/new.html",
+      controller: 'NewYogurtCtrl'
+    })
+    .state('edit', {
+      url: "/yogurts/:id/edit",
+      templateUrl: "templates/edit.html",
+      controller: 'EditYogurtCtrl'
+    })
+    .state('show', {
+      url: "/yogurts/:id",
+      templateUrl: "templates/show.html",
+      controller: 'ShowYogurtCtrl'
+    })
+
+    $urlRouterProvider.otherwise("/yogurts");
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.StatusBar) {
@@ -13,3 +43,7 @@ angular.module('Froyonic', ['ionic'])
     }
   });
 });
+
+
+yogurtApp.controller('YogurtsCtrl', ['$scope', function($scope) {
+}]);
